4e152d277d1d24650269394d4c94ab28451ee446
Michael Kintscher <michael@kintscher.dev>
2023-01-18 15:16:01 -0700

    Tied time highligh on angles chart to input slider.

--- scripts/main.js
+++ scripts/main.js
@@ -53,38 +53,103 @@ var customXScale = d3.scaleLinear().range([0, width]).domain([0, 120])
 const distScale = d3.scaleLinear().range([height, 0]);
 
 const highlightRanges = [
-	{ start: 25, end: 30, color: "crimson", opacity: 0.2, showOpacity: 1.0, label: "Separate Space" },
-	{ start: 87, end: 90, color: "darkgreen", opacity: 0.2, showOpacity: 1.0, label: "Mixed Space" },
-	{ start: 105, end: 110, color: "blue", opacity: 0.2, showOpacity: 1.0, label: "Same Space" }
+	//{ id: "Separate Space", range: { start: 25, end: 30, color: "crimson", opacity: 0.2, showOpacity: 1.0, label: "Separate Space" }},
+	//{ id: "Mixed Space", range: { start: 87, end: 90, color: "darkgreen", opacity: 0.2, showOpacity: 1.0, label: "Mixed Space" }},
+	//{ id: "Same Space", range: { start: 105, end: 110, color: "blue", opacity: 0.2, showOpacity: 1.0, label: "Same Space" } },
+	{ id: "Slider", range: { start: 105, end: 110, color: "blue", opacity: 0.2, showOpacity: 1.0, label: "Selection" } },
 ];
 
-drawHighlightChart(4, 1, ".chartContainer", highlightRanges);
-drawPositionHighlightChart(4, 1, ".chartContainer", highlightRanges);
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var anglesSvg = d3.select(".chartContainer").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform",
+		"translate(" + margin.left + "," + margin.top + ")");
+
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var positionsSvg = d3.select(".chartContainer").append("svg")
+	.attr("width", pos_width + pos_margin.left + pos_margin.right)
+	.attr("height", pos_height + pos_margin.top + pos_margin.bottom)
+	.append("g")
+	.attr("transform",
+		"translate(" + pos_margin.left + "," + pos_margin.top + ")");
+
+updateCharts(4, 1, ".chartContainer", highlightRanges);
+
+// =============================================
+
+
+// =============== INPUT HANDLERS ===============
+
+const startSlider = document.getElementById("startTimeSlider");
+
+startSlider.oninput = function () {
+	//console.log("Slider updated to: " + startSlider.value);
+
+	// Update the highlight ranges (model/data).
+	updateSliderHighlightRange("Slider", startSlider.value, 110);
+
+	// Update each chart based on the new ranges (view).
+	updateTimeHighlight();
+	updatePositionHighlight();
+}
 
 
 // =============================================
 
 
-// ============= HELPER FUNCTIONS ==============
+// =========== DRAW CHART FUNCTIONS ============
 
-async function drawHighlightChart(teamNum, trialNum, containingDiv, ranges) {
-	// append the svg obgect to the body of the page
-	// appends a 'group' element to 'svg'
-	// moves the 'group' element to the top left margin
-	var svg = d3.select(containingDiv).append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform",
-			"translate(" + margin.left + "," + margin.top + ")");
+function updateCharts(teamNum, trialNum, containingDiv, ranges) {
 
+	// Clear the existing charts.
+	anglesSvg.selectAll("g").remove();
+
+	drawHighlightChart(teamNum, trialNum, ranges);
+	drawPositionHighlightChart(teamNum, trialNum, containingDiv, ranges);
+}
+
+function drawHighlightRectangles(chart, ranges) {
+
+	// Append the given ranges to the chart.
+	chart.selectAll("rect")
+		.attr("class", "highlightRange")
+		.data(ranges)
+		.enter().append('rect')
+		.attr("width", function (d) { return customXScale(d.range.end - d.range.start); })
+		.attr("height", height)
+		.attr("x", function (d) { return customXScale(d.range.start); })
+		.attr("y", 0)
+		.style("fill", function (d) { return d.range.color; })
+		.style("opacity", function (d) { return d.range.opacity; });
+}
+
+function updateTimeHighlight() {
+	// Clear the previous rectangles.
+	anglesSvg.selectAll(".highlightRange").remove();
+
+	// Update the new rectangles.
+	drawHighlightRectangles(anglesSvg, highlightRanges);
+}
+
+function updatePositionHighlight() {
+
+}
+
+async function drawHighlightChart(teamNum, trialNum, ranges) {
+	
 	const data = await d3.csv("data/Angles_Team" + teamNum + "_Trial" + trialNum + ".csv");
 	//console.log(data);
 
 	// Format the data (parsing the strings to their appropriate datatypes)
 	data.forEach(function (d) {
 		d.time = parseTime(d.time);
-		console.log(d);
+		//console.log(d);
 		d.p1_angle = Number(d.p1_angle);
 		d.p2_angle = Number(d.p2_angle);
 		d.distance = Number(d.distance);
@@ -117,14 +182,16 @@ async function drawHighlightChart(teamNum, trialNum, containingDiv, ranges) {
 		.y((d) => { return distScale(d.distance); })
 	//.curve(d3.curveCardinal);
 
-	svg.append("path").datum(data)
+	var svgPaths = anglesSvg.append("g");
+
+	svgPaths.append("path").datum(data)
 		.attr("fill", "none")
 		.attr("stroke", legendData[0].color)
 		.attr("stroke-linejoin", "round")
 		.attr("stroke-linecap", "round")
 		.attr("stroke-width", 1.5)
 		.attr("d", p1_line);
-	svg.append("path").datum(data)
+	svgPaths.append("path").datum(data)
 		.attr("fill", "none")
 		.attr("stroke", legendData[1].color)
 		.attr("stroke-linejoin", "round")
@@ -132,7 +199,7 @@ async function drawHighlightChart(teamNum, trialNum, containingDiv, ranges) {
 		.attr("stroke-width", 1.5)
 		.attr("d", p2_line);
 
-	svg.append("path").datum(data)
+	svgPaths.append("path").datum(data)
 		.attr("fill", "none")
 		.attr("stroke", legendData[2].color)
 		.attr("stroke-linejoin", "round")
@@ -142,7 +209,7 @@ async function drawHighlightChart(teamNum, trialNum, containingDiv, ranges) {
 		.style("opacity", 0.8)
 		.attr("d", chart_line);
 
-	svg.append("path").datum(data)
+	svgPaths.append("path").datum(data)
 		.attr("fill", "none")
 		.attr("stroke", legendData[3].color)
 		.attr("stroke-linejoin", "round")
@@ -153,7 +220,7 @@ async function drawHighlightChart(teamNum, trialNum, containingDiv, ranges) {
 		.attr("d", dist_line);
 
 	// Add the threshold line.
-	svg.append("line")
+	svgPaths.append("line")
 		.attr("class", "y")
 		.attr("stroke", legendData[4].color)
 		.style("stroke-dasharray", "3,3")
@@ -166,28 +233,29 @@ async function drawHighlightChart(teamNum, trialNum, containingDiv, ranges) {
 	var customXScale = d3.scaleLinear().range([0, width]).domain([0, 120])
 
 	// Append the highlight ranges.
-	svg.selectAll("rect")
+	svgPaths.selectAll("rect")
+		.attr("class", "highlightRange")
 		.data(ranges)
 		.enter().append('rect')
-		.attr("width", function (d) { return customXScale(d.end - d.start); })
+		.attr("width", function (d) { return customXScale(d.range.end - d.range.start); })
 		.attr("height", height)
-		.attr("x", function (d) { return customXScale(d.start); })
+		.attr("x", function (d) { return customXScale(d.range.start); })
 		.attr("y", 0)
-		.style("fill", function (d) { return d.color; })
-		.style("opacity", function (d) { return d.opacity; });
+		.style("fill", function (d) { return d.range.color; })
+		.style("opacity", function (d) { return d.range.opacity; });
 
 	// Define the x Axis
 	const xAxis = d3.axisBottom(customXScale)
 	//.ticks(11, ".0s");
 
 	// Add the X Axis
-	svg.append("g")
+	anglesSvg.append("g")
 		.attr("transform", "translate(0," + height + ")")
 		.attr("font-family", "Lato")
 		.call(xAxis);
 
 	// Add the X Axis Label
-	svg.append("text")
+	anglesSvg.append("text")
 		.attr("font-family", "sans-serif")
 		.attr("font-size", 14)
 		.attr("font-weight", 700)
@@ -197,11 +265,11 @@ async function drawHighlightChart(teamNum, trialNum, containingDiv, ranges) {
 		.text("Time (seconds)");
 
 	// Add the Y Axis
-	svg.append("g")
+	anglesSvg.append("g")
 		.call(d3.axisLeft(yScale));
 
 	// Add the Y Axis Label
-	svg.append("text")
+	anglesSvg.append("text")
 		.attr("font-family", "sans-serif")
 		.attr("font-size", 14)
 		.attr("font-weight", 700)
@@ -212,7 +280,7 @@ async function drawHighlightChart(teamNum, trialNum, containingDiv, ranges) {
 		.text("Angle (degrees)");
 
 	// Add the title
-	svg.append("text")
+	anglesSvg.append("text")
 		.attr("font-family", "sans-serif")
 		.attr("font-size", 16)
 		.attr("font-weight", 700)
@@ -223,7 +291,7 @@ async function drawHighlightChart(teamNum, trialNum, containingDiv, ranges) {
 		.text("Participant Angles: Team " + teamNum + " Trial " + trialNum);
 
 	// Add the legend
-	var legend = svg.append("g")
+	var legend = anglesSvg.append("g")
 		.attr("transform", "translate(" + (width - 100) + "," + 25 + ")");
 
 	var legendRect = legend.selectAll('g')
@@ -251,15 +319,6 @@ async function drawHighlightChart(teamNum, trialNum, containingDiv, ranges) {
 }
 
 async function drawPositionHighlightChart(teamNum, trialNum, containingDiv, ranges) {
-	// append the svg obgect to the body of the page
-	// appends a 'group' element to 'svg'
-	// moves the 'group' element to the top left margin
-	var svg = d3.select(containingDiv).append("svg")
-		.attr("width", pos_width + pos_margin.left + pos_margin.right)
-		.attr("height", pos_height + pos_margin.top + pos_margin.bottom)
-		.append("g")
-		.attr("transform",
-			"translate(" + pos_margin.left + "," + pos_margin.top + ")");
 
 	const data = await d3.csv("data/Angles_Team" + teamNum + "_Trial" + trialNum + ".csv");
 	//console.log(data);
@@ -283,15 +342,15 @@ async function drawPositionHighlightChart(teamNum, trialNum, containingDiv, rang
 		.range([pos_height, 0])
 		.domain([0, 5]);
 
-	svg.selectAll("dot")
+	positionsSvg.selectAll("dot")
 		.data(data)
 		.enter()
 		.append("circle")
 		.attr("fill", function (d) {
 			var opacity;
 			for (let index in highlightRanges) {
-				if (xScale(d.time) >= customXScale(highlightRanges[index].start) && xScale(d.time) <= customXScale(highlightRanges[index].end)) {
-					opacity = highlightRanges[index].color;
+				if (xScale(d.time) >= customXScale(highlightRanges[index].range.start) && xScale(d.time) <= customXScale(highlightRanges[index].range.end)) {
+					opacity = highlightRanges[index].range.color;
 				}
 			};
 
@@ -304,23 +363,23 @@ async function drawPositionHighlightChart(teamNum, trialNum, containingDiv, rang
 		.style("opacity", function (d, r) {
 			var opacity = 0.1;
 			for (let index in highlightRanges) {
-				if (xScale(d.time) >= customXScale(highlightRanges[index].start) && xScale(d.time) <= customXScale(highlightRanges[index].end)) {
-					opacity = highlightRanges[index].showOpacity;
+				if (xScale(d.time) >= customXScale(highlightRanges[index].range.start) && xScale(d.time) <= customXScale(highlightRanges[index].range.end)) {
+					opacity = highlightRanges[index].range.showOpacity;
 				}
 			};
 
 			return opacity;
 		});
 
-	svg.selectAll("rect")
+	positionsSvg.selectAll("rect")
 		.data(data)
 		.enter()
 		.append("rect")
 		.attr("fill", function (d) {
 			var opacity;
 			for (let index in highlightRanges) {
-				if (xScale(d.time) >= customXScale(highlightRanges[index].start) && xScale(d.time) <= customXScale(highlightRanges[index].end)) {
-					opacity = highlightRanges[index].color;
+				if (xScale(d.time) >= customXScale(highlightRanges[index].range.start) && xScale(d.time) <= customXScale(highlightRanges[index].range.end)) {
+					opacity = highlightRanges[index].range.color;
 				}
 			};
 
@@ -334,8 +393,8 @@ async function drawPositionHighlightChart(teamNum, trialNum, containingDiv, rang
 		.style("opacity", function (d, r) {
 			var opacity = 0.1;
 			for (let index in highlightRanges) {
-				if (xScale(d.time) >= customXScale(highlightRanges[index].start) && xScale(d.time) <= customXScale(highlightRanges[index].end)) {
-					opacity = highlightRanges[index].showOpacity;
+				if (xScale(d.time) >= customXScale(highlightRanges[index].range.start) && xScale(d.time) <= customXScale(highlightRanges[index].range.end)) {
+					opacity = highlightRanges[index].range.showOpacity;
 				}
 			};
 
@@ -347,7 +406,7 @@ async function drawPositionHighlightChart(teamNum, trialNum, containingDiv, rang
 	var chart_size = positionScatterPlotXScale(-1.5);
 
 	// Append the chart icon.
-	svg.append('rect')
+	positionsSvg.append('rect')
 		.attr("width", chart_size)
 		.attr("height", chart_size)
 		.attr("x", positionScatterPlotXScale(chart_x) - 0.5 * chart_size)
@@ -356,7 +415,7 @@ async function drawPositionHighlightChart(teamNum, trialNum, containingDiv, rang
 		.style("opacity", 0.2);
 
 	// Add the chart lines.
-	svg.append("line")
+	positionsSvg.append("line")
 		.attr("class", "y")
 		.attr("stroke", legendData[3].color)
 		.style("stroke-dasharray", "3,3")
@@ -366,7 +425,7 @@ async function drawPositionHighlightChart(teamNum, trialNum, containingDiv, rang
 		.attr("y1", 0)
 		.attr("y2", pos_height);
 
-	svg.append("line")
+	positionsSvg.append("line")
 		.attr("class", "y")
 		.attr("stroke", legendData[3].color)
 		.style("stroke-dasharray", "3,3")
@@ -381,13 +440,13 @@ async function drawPositionHighlightChart(teamNum, trialNum, containingDiv, rang
 	//.ticks(11, ".0s");
 
 	// Add the X Axis
-	svg.append("g")
+	positionsSvg.append("g")
 		.attr("transform", "translate(0," + pos_height + ")")
 		.attr("font-family", "Lato")
 		.call(xAxis);
 
 	// Add the X Axis Label
-	svg.append("text")
+	positionsSvg.append("text")
 		.attr("font-family", "sans-serif")
 		.attr("font-size", 14)
 		.attr("font-weight", 700)
@@ -397,11 +456,11 @@ async function drawPositionHighlightChart(teamNum, trialNum, containingDiv, rang
 		.text("X Position (meters)");
 
 	// Add the Y Axis
-	svg.append("g")
+	positionsSvg.append("g")
 		.call(d3.axisLeft(positionScatterPlotYScale));
 
 	// Add the Y Axis Label
-	svg.append("text")
+	positionsSvg.append("text")
 		.attr("font-family", "sans-serif")
 		.attr("font-size", 14)
 		.attr("font-weight", 700)
@@ -412,7 +471,7 @@ async function drawPositionHighlightChart(teamNum, trialNum, containingDiv, rang
 		.text("Y Position (meters)");
 
 	// Add the title
-	svg.append("text")
+	positionsSvg.append("text")
 		.attr("font-family", "sans-serif")
 		.attr("font-size", 16)
 		.attr("font-weight", 700)
@@ -423,7 +482,7 @@ async function drawPositionHighlightChart(teamNum, trialNum, containingDiv, rang
 		.text("Participant Positions: Team " + teamNum + " Trial " + trialNum);
 
 	// Add the legend
-	var legend = svg.append("g")
+	var legend = positionsSvg.append("g")
 		.attr("transform", "translate(" + (pos_width - 100) + "," + 25 + ")");
 
 	var legendRect = legend.selectAll('g')
@@ -439,7 +498,7 @@ async function drawPositionHighlightChart(teamNum, trialNum, containingDiv, rang
 		.append('rect')
 		.attr("width", 15)
 		.attr("height", 15)
-		.style("fill", function (d) { return d.color; });
+		.style("fill", function (d) { return d.range.color; });
 
 	legendRectE
 		.append("text")
@@ -447,7 +506,21 @@ async function drawPositionHighlightChart(teamNum, trialNum, containingDiv, rang
 		.attr("y", 10)
 		.attr("font-family", "sans-serif")
 		.attr("font-size", 11)
-		.text(function (d) { return d.label; });
+		.text(function (d) { return d.range.label; });
+}
+
+// =============================================
+
+
+// ============= HELPER FUNCTIONS ==============
+
+function updateSliderHighlightRange(rangeId, startTime, endTime) {
+	// Get a reference to the range to update.
+	var range = highlightRanges.find(r => { return r.id == rangeId; });
+	console.log("Found range: " + range.id);
+	// Update the start and end time of the given range.
+	range.range.start = startTime;
+	range.range.end = endTime;
 }
 
 // =============================================
